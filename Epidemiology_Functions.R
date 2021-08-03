4048d5594858650056c406d52f2f8680446c626c
TimBosman <50997599+TimBosman@users.noreply.github.com>
2021-08-03 09:39:27 +0200

    Update Epidemiology_Functions.R
    Small updates on the Epidemiology_Functions.R

--- Epidemiology_Functions.R
+++ Epidemiology_Functions.R
@@ -3,6 +3,7 @@ library(tidyr)
 library(plyr)
 library(ggplot2)
 library(patchwork)
+library(lme4)
 
 ### Functions #################################################################
 
@@ -27,8 +28,6 @@ get_pedigree <- function(nsires, ndams = NA, OffspringPerMate = 1,
 
 add_trait_to_pedigree <- function(name, vA, vE, pedigree, SireBVFile = NA, 
                                   DamBVFile = NA){
-  # nsires <- length(levels(pedigree$sires))
-  # mates <- nrow(pedigree) / nsires
   Adams <- rnorm(length(levels(pedigree$dams)), 0, sqrt(vA))
   Asire <- rnorm(length(levels(pedigree$sires)), 0, sqrt(vA))
   vMS <- 0.5 * vA
@@ -39,12 +38,13 @@ add_trait_to_pedigree <- function(name, vA, vE, pedigree, SireBVFile = NA,
   pedigree <- cbind(pedigree, trait, Arec)
   names(pedigree)[names(pedigree) %in%  c("trait", "Arec")] <- 
     c(name, paste("breeding value of", name))
-  
+  #If the BV for the sires are required later save them in a file
   if (!is.na(SireBVFile)) {
     df <- unique.data.frame(cbind(pedigree$sires, Asire[pedigree$sires]))
     colnames(df) <- c("Sire ID", paste("BV for", name))
     write.csv(df, SireBVFile, row.names = FALSE)
   }
+  #Same for dams
   if (!is.na(DamBVFile)){
     df <- unique.data.frame(cbind(pedigree$dams, Adams[pedigree$dams]))
     colnames(df) <- c("Dam ID", paste("BV for", name))
@@ -56,6 +56,8 @@ add_trait_to_pedigree <- function(name, vA, vE, pedigree, SireBVFile = NA,
 set_herd <- function(pedigree, nherds, off_sire_g, nsires_g){
   #Based on script of Dries Hulst
   #Download on: https://doi.org/10.25386/genetics.13090076 (S4)
+  #WARINING only works for nherds == nsires
+  if(nherds != length(levels(pedigree$sires))) stop("This algorithm is not prepared for situations where nherds != nsires")
   samples_sg <- matrix(nrow = nherds, ncol = nsires_g)
   samples_sg[,1] <- sample(1:nherds, nherds, replace = FALSE)
   for (s in 2:nsires_g){
@@ -160,7 +162,9 @@ Plot_time_series <- function(TimeSeries, timepoints){
   temp$value[temp$value == "R"] <- "Recovered"
   temp$value[temp$value == "I"] <- "Infected"
   temp$value[temp$value == "S"] <-  "Susceptible"
-  ggplot(temp) + geom_bar(aes(x= Time, fill = value), stat = "count") 
+  temp$value = factor(temp$value, levels = c("Susceptible", "Infected", "Recovered"))
+  ggplot(temp) + geom_bar(aes(x= Time, fill = value), stat = "count") +
+  ylab("Number of infected individuals")
 }
 
 Plot_infected_fraction <- function(events, herds, timelimit){
@@ -179,12 +183,13 @@ Write_infectivity_file_for_SIRE <- function(pedigree, filename, timepoints){
   pedigree$type <- "Contact"
   pedigree$type[pedigree$`0` == "I"] <- "Seeder"
   pedigree <- pivot_longer(pedigree, as.character(timepoints), "time")
+  pedigree <- pedigree[, c("offspring", "sires", "herd", "time", "type", "value")]
   write.table(pedigree, file = filename, sep = "\t", row.names = FALSE, quote = FALSE)
 }
 
 generate_GLMM_Data <- function(InfectedPedigree, timepoints){
-  dif = data.frame(matrix(NA, ncol = 8))
-  names(dif)<- c("Herd", "Sire", "S", "C", "DeltaT", "I", "t", "Isire")
+  dif = data.frame(matrix(NA, ncol = 9))
+  names(dif)<- c("Herd", "Sire", "S", "C", "DeltaT", "I", "t", "Isire" , "R")
   row = 1
   for (herd in levels(InfectedPedigree$herd)){
     herdrows = which(InfectedPedigree$herd == herd)
@@ -199,7 +204,8 @@ generate_GLMM_Data <- function(InfectedPedigree, timepoints){
         Isire = sum(t1_value == "I")
         S = sum(t1_value == "S")
         C = S - sum(t2_value == "S")
-        dif[row, ] = c(herd, sire, S, C, DeltaT, I, timepoint, Isire)
+        R = sum(t2_value == "R") - sum(t1_value == "R")
+        dif[row, ] = c(herd, sire, S, C, DeltaT, I, timepoint, Isire, R)
         row = row + 1
       }
     }
@@ -211,6 +217,115 @@ generate_GLMM_Data <- function(InfectedPedigree, timepoints){
   dif$I <- as.integer(dif$I)
   dif$DeltaT <- as.numeric(dif$DeltaT)
   dif$Isire <- as.integer(dif$Isire)
+  dif$R <- as.integer(dif$R)
   
   return(dif)
+}
+
+addInfectedSiresInHerd <- function(GLMM_Data){
+  for (row in 1:nrow(GLMM_Data)){
+    rows = which(GLMM_Data$Herd == GLMM_Data$Herd[row] & GLMM_Data$t == GLMM_Data$t[row])
+    count = 1
+    for (sire in GLMM_Data$Sire[rows]){
+      GLMM_Data[rows, paste0("sire", count)] = paste(sire)
+      GLMM_Data[rows, paste0("I", count)] = GLMM_Data[rows[rows %in% which(GLMM_Data$Sire == sire)], "Isire"]
+      count = count + 1
+    }
+  }
+  return(GLMM_Data)
+}
+
+# quickRun <- function(nsire, ndams, nherds,nSiresPerHerd, nOffspringPerHerd, vAsus, vEsus, vAinf,vEinf, alpha, contactrate, timepoints){
+#   pedigree <- get_pedigree(nsire, ndams)
+#   pedigree <- add_trait_to_pedigree("susceptibility", vAsus, vEsus, pedigree, 
+#                                     SireBVFile = "BVsus.csv")
+#   pedigree <- add_trait_to_pedigree("infectivity", vAinf, vEinf, pedigree, 
+#                                     SireBVFile = "BVinf.csv")
+#   pedigree <- set_herd(pedigree, nherds, nOffspringPerHerd, nSiresPerHerd)
+#   
+#   ## Simulate infection for every herd ##
+#   InfectedPedigree <- data.frame()
+#   events <- data.frame()
+#   for(herd in levels(pedigree$herd)){
+#     herddata <- pedigree[pedigree$herd == herd,]
+#     repeat{
+#       output <- simulate_infection(herddata, alpha, contactrate, max(timepoints),
+#                                    infectivity = herddata$infectivity,
+#                                    susceptibility = herddata$susceptibility,
+#                                    initialstate = sample(c(rep("S", 99), "I")))
+#       if(nrow(output[[2]]) > 2){
+#         break
+#       }
+#     }
+#     InfectedPedigree <- rbind(InfectedPedigree, output[[1]])
+#     events <- rbind(events, output[[2]])
+#   }
+#   rm(pedigree, output, herddata)
+#   ## Generate timeseries barplot ##
+#   InfectedPedigree <- Generate_time_series_data(timepoints, events, InfectedPedigree)
+#   
+#   # Plot_time_series(InfectedPedigree, timepoints)
+#   # 
+#   # Plot_infected_fraction(events, 1:nherds, max(timepoints))
+#   rm(events)
+#   # Write_infectivity_file_for_SIRE(InfectedPedigree, "Scenario1.txt", timepoints)
+#   
+#   GLMM_Data <- generate_GLMM_Data(InfectedPedigree, timepoints)
+#   
+#   GLMM_Data <- addInfectedSiresInHerd(GLMM_Data)
+#   return(GLMM_Data)
+# }
+
+GetEstimations <- function(bvFile, Sirefile, GLMM_model, sireCode, glmmterm){
+  #Get real BVs
+  data <- read.csv(bvFile)
+  
+  #Get SIRE estimates
+  sireEstimates = read.table(Sirefile, header = TRUE)
+  SIREestimates = sapply(c(2:100), function(i){
+    mean(sireEstimates[,paste0(sireCode, i)])
+  })
+  SIREestimates <- c(0, SIREestimates)
+  data$SIRE <- SIREestimates
+  
+  #Get GLMM estimates
+  data2 <-data.frame(ranef(GLMM_model)[glmmterm])
+  data$GLMM <- data2[match(data$Sire.ID, rownames(data2)),1]
+  return(data)
+}
+
+glmer_multimemb <- function(formula, data, memb_mat=list(), herdsize, ...) {
+  data$herdsize <- herdsize
+  mnms <- names(memb_mat)
+  fb <- findbars(formula)
+  gvars <- vapply(fb, function(x) deparse(x[[3]]), character(1))
+  Ztlist <- list()
+  for (i in seq_along(fb)) {
+    fbnm <- deparse(fb[[i]])
+    ## find corresponding random-effects term
+    w <- which(mnms==gvars[i])
+    if (length(w)>0) {
+      M <- Matrix::Matrix(memb_mat[[w]])
+      ## extract LHS (effect)
+      form <- as.formula(substitute(~z,list(z=fb[[i]][[2]])))
+      ## construct model matrix & compute Khatri-Rao product
+      X <- model.matrix(form,data=data)
+      Zt <- Matrix::KhatriRao(t(M),t(X),make.dimnames=TRUE)
+      ## FIXME: mess with names?
+      Ztlist[[fbnm]] <- Zt
+    } ## if  (length(w)>0)
+  } ## for i in seq(fb)
+  lmod <- lFormula(formula,data=data)
+  ## substitute new Ztlist elements
+  # m = names(Ztlist)
+  for (m in names(Ztlist)) {
+    lmod$reTrms$Ztlist[[m]] <- Ztlist[[m]]
+  }
+  lmod$reTrms$Zt <- do.call(rbind,lmod$reTrms$Ztlist)
+  ## finish fitting
+  m1 <- glmer(lmod,
+              family = binomial(link="cloglog"),
+              data = data,
+              offset = log(data$I / herdsize * data$DeltaT))
+  return(m1)
 }
